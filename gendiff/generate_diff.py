d84c34f90df24c79042ff39fb9b27e06a1e5d8dc
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-07-15 22:23:55 +0600

    Refactor generate_diff func
    I have much trouble with output comparison string. I remake this func from scratch and set intends to 4 Spaces by linter's advice.

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,20 +2,24 @@ import json
 
 
 def generate_diff(file_path1, file_path2):
-     file1 = json.load(open(file_path1))
-     file2 = json.load(open(file_path2))
-     result = '{' + '\n'
-     for i in file1.keys() & file2.keys():
-          if file1[i] == file2[i]:
-               result = result + '   ' + i + ': ' + str(file1[i]) + '\n'
-          else:
-               result = result + ' + ' + i + ': ' + str(file2[i]) + '\n'
-               result = result + ' - ' + i + ': ' + str(file1[i]) + '\n'
-     for i in file1.keys() - file2.keys():
-          result = result + ' - ' + i + ': ' + str(file1[i]) + '\n'
-     for i in file2.keys() - file1.keys():
-          result = result + ' + ' + i + ': ' + str(file2[i]) + '\n'
-     result = result + '}'
-     return result
+    file1 = json.load(open(file_path1))
+    file2 = json.load(open(file_path2))
+    merged_files = file1 | file2
+    sorted_files = dict(sorted(merged_files.items(), key=lambda x: x[0]))
+    result = '{' + '\n'
+    for i in sorted_files:
+        if i in file1.keys() & file2.keys():
+            if file1[i] == file2[i]:
+                result = result + '   ' + i + ': ' + str(file1[i]) + '\n'
+            else:
+                result = result + ' - ' + i + ': ' + str(file1[i]) + '\n'
+                result = result + ' + ' + i + ': ' + str(file2[i]) + '\n'
+        elif i in file1.keys() - file2.keys():
+            result = result + ' - ' + i + ': ' + str(file1[i]) + '\n'
+        elif i in file2.keys() - file1.keys():
+            result = result + ' + ' + i + ': ' + str(file2[i]) + '\n'
+    result = result + '}'
+    return result.lower()
+
 
 # print(generate_diff(r'gendiff/files/file1.json', r'gendiff/files/file2.json'))
