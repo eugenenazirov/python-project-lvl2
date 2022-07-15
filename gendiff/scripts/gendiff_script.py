307f05f69a6c631d257cdebfc768284caba4492c
Евгений Назиров <eugenenazirov@MacBook-Air-Evgenij.local>
2022-07-15 13:48:30 +0600

    Rename script with "_script" postfix

---
+++ gendiff/scripts/gendiff_script.py
@@ -0,0 +1,17 @@
+from gendiff import generate_diff
+import argparse
+
+
+def main():
+    gendiff = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files\
+     and shows a difference.')
+    gendiff.add_argument('first_file')
+    gendiff.add_argument('second_file')
+    gendiff.add_argument('-f', '--format', help='set format of output')
+    args = gendiff.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
+
+
+if __name__ == '__main__':
+    main()
